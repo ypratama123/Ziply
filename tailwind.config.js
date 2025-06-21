efb052f10dbac16dba526f2c304a73143cd75329
Yoga pratama <y4.yogapratama123@gmail.com>
2025-06-22 04:45:32 +0700

    fix: perbaiki animasi FloatingShapes yang tidak muncul

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        float: "float 12s ease-in-out infinite",
+      },
+      keyframes: {
+        float: {
+          "0%, 100%": {
+            transform: "translateY(0px) rotate(0deg)",
+          },
+          "50%": {
+            transform: "translateY(-25px) rotate(15deg)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 } 
